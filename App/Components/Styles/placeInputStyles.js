1489cebb8dc2cb98cff98395f2b87fe77f14e9c4
ThakurKarthik <iamkarthik08@gmail.com>
2019-10-20 01:09:37 +0530

    input place ui

--- App/Components/Styles/placeInputStyles.js
+++ App/Components/Styles/placeInputStyles.js
@@ -2,6 +2,24 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   inputWrapper: {
-    borderWidth: 1
+    borderWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    margin: 5,
+    borderRadius: 25
+  },
+  input: {
+    width: "90%",
+    paddingLeft: 15
+  },
+  description: {
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 5,
+    marginVertical: 10,
+    textAlign: "center",
+    fontSize: 15,
+    fontWeight: "bold",
+    marginHorizontal: 5
   }
 });
--- App/Containers/first.js
+++ App/Containers/first.js
@@ -1,35 +1,148 @@
 import React, { Component } from "react";
-import { View, StatusBar, Text } from "react-native";
+import { View, StatusBar, Text, PermissionsAndroid } from "react-native";
 // import ReduxNavigation from '../Navigation/ReduxNavigation'
 import { connect } from "react-redux";
 // import StartupActions from '../Redux/StartupRedux'
 import RoundedButtons from "../Components/RoundedButton";
 // Styles
 import styles from "./Styles/firstStyles";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import InputPlace from "../Components/InputPlace";
 class First extends Component {
-  componentDidMount() {
+  state = {
+    pickUpInputActive: false,
+    dropInputActive: false,
+    location: {
+      pickUp: {},
+      drop: {}
+    },
+    extraData: {
+      pickUp: "",
+      drop: ""
+    },
+    initialRegion: null
+  };
+  async componentDidMount() {
     // this.props.startup()
+    this.requestCameraPermission();
+    this.getCurrentLocation();
   }
 
+  requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: "Please grant location",
+          message: "Enabling this permission helps to get current location"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the location");
+      } else {
+        console.log("Location permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+  async getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        let region = {
+          latitude: parseFloat(position.coords.latitude),
+          longitude: parseFloat(position.coords.longitude),
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05
+        };
+        this.setState({
+          initialRegion: region
+        });
+      },
+      error => {
+        console.log(error);
+      },
+      {
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: 10000
+      }
+    );
+  }
+  isEmpty = object => Object.keys(object).length == 0;
+  toggleActiveInput = id => {
+    const secondInput =
+      id === "pickUpInputActive" ? "dropInputActive" : "pickUpInputActive";
+    const locationField = id === "pickUpInputActive" ? "drop" : "pickUp";
+    if (this.isEmpty(this.state.location[locationField])) {
+      this.setState(prevState => ({
+        [id]: !prevState[id],
+        [secondInput]: false
+      }));
+    } else {
+      this.setState(prevState => ({
+        [id]: !prevState[id]
+      }));
+    }
+  };
+  updateLocation = (data, extraData, pickOrDrop) => {
+    console.log("data", data, extraData, pickOrDrop);
+    this.setState(
+      {
+        location: {
+          ...this.state.location,
+          [pickOrDrop]: {
+            latitude: data.lat,
+            longitude: data.lng
+          }
+        },
+        extraData: {
+          ...this.state.extraData,
+          [pickOrDrop]: extraData
+        }
+      },
+      () => {
+        this.state;
+      }
+    );
+  };
   render() {
+    console.log(this.state);
     return (
       <View style={styles.applicationView}>
         <StatusBar barStyle="light-content" />
         {/* <ReduxNavigation /> */}
-        <InputPlace placeholder="Pick up point" />
-        <InputPlace placeholder="Drop point" />
+        <InputPlace
+          placeholder="Pick up point"
+          isActive={this.state.pickUpInputActive}
+          toggleActive={() => this.toggleActiveInput("pickUpInputActive")}
+          updateLocation={(data, extraData) =>
+            this.updateLocation(data, extraData, "pickUp")
+          }
+          fillLocation={this.state.location}
+          extraData={this.state.extraData.pickUp}
+        />
+        <InputPlace
+          placeholder="Drop point"
+          isActive={this.state.dropInputActive}
+          toggleActive={() => this.toggleActiveInput("dropInputActive")}
+          updateLocation={(data, extraData) =>
+            this.updateLocation(data, extraData, "drop")
+          }
+          extraData={this.state.extraData.drop}
+        />
         <View style={styles.mapContainer}>
-          {/* <MapView
+          <MapView
             style={styles.mapStyles}
-            initialRegion={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421
-            }}
-          /> */}
+            initialRegion={this.state.initialRegion}
+          >
+            {Object.keys(this.state.location.pickUp).length > 0 && (
+              <Marker coordinate={this.state.location.pickUp} title="pick up" />
+            )}
+            {Object.keys(this.state.location.drop).length > 0 && (
+              <Marker coordinate={this.state.location.drop} title="drop" />
+            )}
+          </MapView>
         </View>
         {/* <Text>Hello world</Text> */}
       </View>
